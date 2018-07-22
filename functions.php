470139008733cee85a04abe75c4aa9aa7dceabc4
Teague Nelson <teague@owlmountian.co>
2018-07-22 09:22:02 -0700

    theme fixes before first launch

--- functions.php
+++ functions.php
@@ -363,3 +363,4 @@ add_action( 'wp_enqueue_scripts', 'sp_enqueue_ionicons' );
 function sp_enqueue_ionicons() {
 	wp_enqueue_style( 'ionicons', '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css', array(), CHILD_THEME_VERSION );
 }
+
