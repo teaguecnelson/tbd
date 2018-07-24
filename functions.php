6fcde195ce3524ddf5015bc580c576bfeb235765
Teague Nelson <teague@owlmountian.co>
2018-07-24 15:09:54 -0700

    sidebar min-width and adding google tag manager

--- functions.php
+++ functions.php
@@ -364,3 +364,23 @@ function sp_enqueue_ionicons() {
 	wp_enqueue_style( 'ionicons', '//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css', array(), CHILD_THEME_VERSION );
 }
 
+// Add Google Tag Manager code in <head>
+add_action( 'wp_head', 'sk_google_tag_manager1' );
+function sk_google_tag_manager1() { ?>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KHZQ34S');</script>
+	<!-- End Google Tag Manager -->
+<?php }
+
+// Add Google Tag Manager second part code immediately below opening <body> tag
+add_action( 'genesis_before', 'sk_google_tag_manager2' );
+function sk_google_tag_manager2() { ?>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KHZQ34S"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+<?php }
